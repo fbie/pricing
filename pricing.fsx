e3a8ac8068f7b1283160637040227e7074a32388
Florian Biermann <florian.biermann@simcorp.com>
2020-10-02 08:46:45 +0200

    Allow unary and binary functions.

--- pricing.fsx
+++ pricing.fsx
@@ -16,7 +16,8 @@ module Pricing =
             | Mul of e_num * e_num
             | Div of e_num * e_num
             | If of e_bool * e_num * e_num
-            | Call of (float -> float) * e_num // Should probably take more args?
+            | Call1 of (float -> float) * e_num
+            | Call2 of (float -> float -> float) * e_num * e_num
 
         let rec eval_bool result = function
             | True -> true
@@ -43,8 +44,10 @@ module Pricing =
                 eval_num result e1 / eval_num result e2
             | If (cond, e1, e2) ->
                 if eval_bool result cond then eval_num result e1 else eval_num result e2
-            | Call (f, e) ->
+            | Call1 (f, e) ->
                 f (eval_num result e)
+            | Call2 (f, e1, e2) ->
+                f (eval_num result e1) (eval_num result e2)
 
    // Test it in the top-level:
    // open Expr
